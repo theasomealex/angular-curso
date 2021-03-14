9128a562e3bf928478839464f67a64a09e6c89de
Fabian Alejandro Vazquez Chavez <fabian.vchavez@ibm.com>
2021-03-14 12:57:54 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+
+import { HeoresModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeoresModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
